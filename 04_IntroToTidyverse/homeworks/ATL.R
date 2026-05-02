4d751bf0142b2ee64518f0bdbefc76baf4926470
Anjana <al2271@cam.ac.uk>
2026-05-02 08:56:50 +0100

    end of the lesson

---
+++ 04_IntroToTidyverse/homeworks/ATL.R
@@ -0,0 +1,14 @@
+#problem1
+#taking a dataset, work toght the colums operating functions
+select (), rename(), relocate()
+#then filter() by conditions on 2 separated colums arrange in order
+#export new file.
+#problem2
+#use mutate to create new colums or modify existing ones
+#use round to get less significant digits
+# use round to do proportions
+#export this file
+#problem3
+#use mutate to convine colums
+# bind, timepoint, condtion
+paste0() to save a new colun name 
