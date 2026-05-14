b30e7b38e8b9b70d1399cddffc256a634a515a4d
Anjana Lopez <al2271@cam.ac.uk>
2026-05-14 13:11:07 +0100

    end HW

--- ATL.R
+++ ATL.R
@@ -3,11 +3,13 @@
 select (), rename(), relocate()
 #then filter() by conditions on 2 separated colums arrange in order
 #export new file.
+
 #problem2
 #use mutate to create new colums or modify existing ones
 #use round to get less significant digits
 # use round to do proportions
 #export this file
+
 #problem3
 #use mutate to convine colums
 # bind, timepoint, condtion
